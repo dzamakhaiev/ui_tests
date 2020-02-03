5f50e72ce305c924807d4058ea2ec37f9fba1b0a
dzamakhaiev <bogart047@gmail.com>
2020-02-03 20:34:17 +0200

    Add initial methods

--- pages.py
+++ pages.py
@@ -1,5 +1,17 @@
+from selenium import webdriver
 
 
 class BasePage:
-    pass
+
+    def __init__(self, driver):
+        """
+        :param webdriver.Chrome driver: added to help with auto fill
+        """
+        self.driver = driver
+
+    def go_to(self, url):
+        return self.driver.get(url=url)
+
+    def find_element(self, by, value):
+        return self.driver.find_element(by=by, value=value)
 
