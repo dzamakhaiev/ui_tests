118a4a20bddedb08ad7af4560af6acb26c9d1761
dzamakhaiev <bogart047@gmail.com>
2020-02-03 20:34:39 +0200

    add initial tests

--- tests.py
+++ tests.py
@@ -1,4 +1,24 @@
 import unittest
+from selenium import webdriver
+import config
+import test_data
+
+
+class ChromeTest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=config.CHROMEDRIVER_PATH)
+        self.driver.implicitly_wait(config.IMP_WAIT_TIME)
+        self.driver.maximize_window()
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test(self):
+        pass
+
+    def test1(self):
+        pass
 
 
 if __name__ == '__main__':
