be46d0cae6a6bf519b4d3268d29340071ffb6254
dzamakhaiev <bogart047@gmail.com>
2020-02-04 16:54:34 +0200

    Add methods and classes

--- pages.py
+++ pages.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from locators import *
 
 
 class BasePage:
@@ -12,6 +13,33 @@ class BasePage:
     def go_to(self, url):
         return self.driver.get(url=url)
 
-    def find_element(self, by, value):
-        return self.driver.find_element(by=by, value=value)
+    def find_element(self, locator):
+        return self.driver.find_element(*locator)
 
+    def click_on_element(self, locator):
+        self.find_element(locator).click()
+
+    def input_text(self, locator, text):
+        field = self.find_element(locator)
+        field.send_keys(text)
+
+
+class MainPage(BasePage):
+
+    def click_on_login_link(self):
+        self.click_on_element(MainPageLocators.LOGIN_LINK)
+        return self.driver
+
+
+class LoginPage(BasePage):
+
+    def log_in(self, email, password):
+        self.input_text(locator=LoginPageLocators.EMAIL_FIELD, text=email)
+        self.input_text(locator=LoginPageLocators.PWD_FIELD, text=password)
+        self.click_on_element(LoginPageLocators.LOG_IN_BUTTON)
+
+
+class AccountPage(BasePage):
+
+    def get_user_name(self):
+        return self.find_element(LoginPageLocators.USER_LINK).text
