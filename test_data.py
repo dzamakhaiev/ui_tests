c1ac3b9e938bc90cbc41fb1f426ed0978604a363
dzamakhaiev <bogart047@gmail.com>
2020-02-04 16:40:22 +0200

    Amended user data

--- test_data.py
+++ test_data.py
@@ -1,6 +1,7 @@
 # User and login info
-TEST_USER_EMAIL = 'test.email@gmail.com'
-TEST_PWD = '111111111'
+USER_EMAIL = 'test.mail@gmail.com'
+USER_PWD = '11111111'
+USER_NAME = 'TEST_NAME'
 
 # URLs
 MAIN_PAGE = 'https://elektroshokery.in.ua/'
