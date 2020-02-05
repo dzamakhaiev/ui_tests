726c55671989c673e97cb2993f74d550e733774f
dzamakhaiev <bogart047@gmail.com>
2020-02-05 16:31:17 +0200

    test refactor

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
-from selenium import webdriver
-import config
+import random
+from driver import ChromeDriver
 import test_data
 import pages
 
@@ -8,33 +8,53 @@ import pages
 class ChromeTest(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path=config.CHROMEDRIVER_PATH)
-        self.driver.implicitly_wait(config.IMP_WAIT_TIME)
-        self.driver.maximize_window()
-        self.driver.get(url=test_data.MAIN_PAGE)
+        # Start test case from Main page like regular user
+        self.current_page = pages.MainPage(driver=ChromeDriver(), url=test_data.MAIN_PAGE)
 
-    def tearDown(self):
-        self.driver.quit()
-
-    def test_login(self):
+    def log_in(self):
         # Start from Main page
-        main_page = pages.MainPage(driver=self.driver)
+        driver = self.current_page.get_driver()
+        main_page = pages.MainPage(driver=driver, url=test_data.MAIN_PAGE)
         main_page.click_on_login_link()
 
         # Go to login page
-        login_page = pages.LoginPage(self.driver)
+        driver = self.current_page.get_driver()
+        login_page = pages.LoginPage(driver=driver, url=test_data.LOGIN_PAGE)
         login_page.log_in(email=test_data.USER_EMAIL, password=test_data.USER_PWD)
 
+        # After log in current page will be Account page
+        driver = self.current_page.get_driver()
+        self.current_page = pages.AccountPage(driver=driver, url=test_data.ACCOUNT_PAGE)
+
+    def test_login(self):
+        # Start from Main page and log in to reach Account page
+        self.log_in()
+
         # Check current page after log in
-        self.assertEqual(self.driver.current_url, test_data.ACCOUNT_PAGE)
+        self.assertEqual(self.current_page.get_current_url(), test_data.ACCOUNT_PAGE)
 
         # Check user name after log in
-        acc_page = pages.AccountPage(self.driver)
+        driver = self.current_page.get_driver()
+        acc_page = pages.AccountPage(driver=driver, url=test_data.ACCOUNT_PAGE)
         user_name = acc_page.get_user_name()
         self.assertEqual(user_name, test_data.USER_NAME)
 
-    def test1(self):
-        pass
+    def test_add_to_cart(self):
+        # Start from Main page and log in
+        self.log_in()
+
+        # Turn back to main page
+        driver = self.current_page.get_driver()
+        main_page = pages.MainPage(driver=driver, url=test_data.MAIN_PAGE)
+
+        # Pick up random product
+        products = main_page.get_product_list()
+        product = random.choice(products)
+
+        # Try to add product to cart
+        main_page.add_product_to_cart(product)
+
+        # Check cart
 
 
 if __name__ == '__main__':
