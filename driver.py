9367dbf8936f04496d7fbff11f4b2366fd7a6fa9
dzamakhaiev <bogart047@gmail.com>
2020-02-05 15:08:02 +0200

    add driver classes

--- driver.py
+++ driver.py
@@ -1,13 +1,26 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver import Chrome
 import config
-from selenium import webdriver
 
 
 class Driver:
 
-    def __init__(self, driver_path=config.CHROMEDRIVER_PATH):
-        self.driver = webdriver.Chrome(executable_path=driver_path)
+    def __init__(self, driver):
+        """
+        :param WebDriver driver:
+        """
+        self.driver = driver
         self.driver.implicitly_wait(config.IMP_WAIT_TIME)
         self.driver.maximize_window()
 
     def __del__(self):
-        self.driver.quit()
+        # Cannot use here quit() due to issue: https://github.com/SeleniumHQ/selenium/issues/3330
+        self.driver.close()
+
+
+class ChromeDriver(Driver):
+
+    def __init__(self, driver_path=config.CHROMEDRIVER_PATH):
+        driver = Chrome(executable_path=driver_path)
+        super().__init__(driver)
+
