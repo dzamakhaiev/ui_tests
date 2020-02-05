9ecb6496796cf87b0ccc2369e6a9805d0ec16e84
dzamakhaiev <bogart047@gmail.com>
2020-02-05 15:24:07 +0200

    Changed class for work with Driver class

--- pages.py
+++ pages.py
@@ -1,45 +1,47 @@
-from selenium import webdriver
-from locators import *
+from driver import Driver
+import locators
 
 
 class BasePage:
 
-    def __init__(self, driver):
+    def __init__(self, driver, url):
         """
-        :param webdriver.Chrome driver: added to help with auto fill
+        :param Driver driver: object for work with webdriver
+        :param str url: base url for that page
         """
         self.driver = driver
-
-    def go_to(self, url):
-        return self.driver.get(url=url)
-
-    def find_element(self, locator):
-        return self.driver.find_element(*locator)
-
-    def click_on_element(self, locator):
-        self.find_element(locator).click()
-
-    def input_text(self, locator, text):
-        field = self.find_element(locator)
-        field.send_keys(text)
+        self.base_url = url
 
 
 class MainPage(BasePage):
 
     def click_on_login_link(self):
-        self.click_on_element(MainPageLocators.LOGIN_LINK)
+        self.driver.click_on_element(locators.MainPageLocators.LOGIN_LINK)
         return self.driver
+
+    def get_product_list(self):
+        return self.driver.find_elements(locators.MainPageLocators.PRODUCT_BLOCK)
+
+    def add_product_to_cart(self, product):
+        self.driver.click_on_element(locator=locators.MainPageLocators.BUY_BUTTON, element=product)
+
+    def get_cart_total(self):
+        return self.driver.find_element(locators.MainPageLocators.CART_TOTAL).text
 
 
 class LoginPage(BasePage):
 
     def log_in(self, email, password):
-        self.input_text(locator=LoginPageLocators.EMAIL_FIELD, text=email)
-        self.input_text(locator=LoginPageLocators.PWD_FIELD, text=password)
-        self.click_on_element(LoginPageLocators.LOG_IN_BUTTON)
+        self.driver.input_text(locator=locators.LoginPageLocators.EMAIL_FIELD, text=email)
+        self.driver.input_text(locator=locators.LoginPageLocators.PWD_FIELD, text=password)
+        self.driver.click_on_element(locators.LoginPageLocators.LOG_IN_BUTTON)
 
 
 class AccountPage(BasePage):
 
     def get_user_name(self):
-        return self.find_element(LoginPageLocators.USER_LINK).text
+        return self.driver.find_element(locators.LoginPageLocators.USER_LINK).text
+
+
+class CartPage(BasePage):
+    pass
