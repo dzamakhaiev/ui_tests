0f4e9c7f522881a066444f6cccc54595853cb7de
dzamakhaiev <bogart047@gmail.com>
2020-02-05 15:16:33 +0200

    replace find methods

--- driver.py
+++ driver.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver import Chrome
 import config
 
@@ -12,6 +13,38 @@ class Driver:
         self.driver = driver
         self.driver.implicitly_wait(config.IMP_WAIT_TIME)
         self.driver.maximize_window()
+
+    def get_driver(self):
+        return self.driver
+
+    def set_driver(self, driver):
+        """
+        :param WebDriver driver:
+        """
+        self.driver = driver
+
+    def go_to(self, url):
+        self.driver.get(url)
+
+    def find_element(self, locator, element=None):
+        """
+        :param tuple locator:
+        :param WebElement element:
+        """
+        if not element:
+            return self.driver.find_element(*locator)
+        else:
+            return element.find_element(*locator)
+
+    def find_elements(self, locator):
+        return self.driver.find_elements(*locator)
+
+    def click_on_element(self, locator, element=None):
+        self.find_element(locator, element).click()
+
+    def input_text(self, locator, text, element=None):
+        field = self.find_element(locator, element)
+        field.send_keys(text)
 
     def __del__(self):
         # Cannot use here quit() due to issue: https://github.com/SeleniumHQ/selenium/issues/3330
