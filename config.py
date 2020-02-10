c9bbabc13fc66e5055c4430c02613af282ba84ae
dzamakhaiev <bogart047@gmail.com>
2020-02-10 12:06:02 +0200

    change calc time

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ CHROMEDRIVER_PATH = r'C:\Users\work\Downloads\chromedriver.exe'
 
 # Driver config
 IMP_WAIT_TIME = 5
-CALCULATING_TIME = 1  # sleep time related to old tech of site
+CALCULATING_TIME = 0.8  # sleep time related to old tech of site
